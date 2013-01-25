595579e6bd5b10d3dbd61ed7fb62a7ffd69d4d85
Peter Berkenbosch <peter@pero-ict.nl>
2013-01-25 14:01:40 +0100

    load the brand by the url.

--- app/controllers/spree/admin/brands_controller.rb
+++ app/controllers/spree/admin/brands_controller.rb
@@ -1,4 +1,11 @@
-class Spree::Admin::BrandsController < Spree::Admin::ResourceController
-  def index
+module Spree
+  module Admin
+    class BrandsController < ResourceController
+      protected
+      def find_resource
+        Spree::Brand.find_by_url!(params[:id])
+      end
+
+    end
   end
 end
