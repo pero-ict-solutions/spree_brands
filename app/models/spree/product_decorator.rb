13dd7aebacab480f18791e416bcfe3a8a7b65ee9
Peter Berkenbosch <peter@pero-ict.nl>
2013-05-03 16:09:42 +0200

    add by_brand scope to be used in search filter

--- app/models/spree/product_decorator.rb
+++ app/models/spree/product_decorator.rb
@@ -2,6 +2,10 @@ Spree::Product.class_eval do
   attr_accessible :brand_id
   belongs_to :brand, :class_name => "Spree::Brand", :foreign_key => "brand_id"
 
+  def self.by_brand(brand_id)
+    where(brand_id: brand_id)
+  end
+
   def self.with_active_brands
     joins(:brand).where(spree_brands: { enabled: true})
   end
